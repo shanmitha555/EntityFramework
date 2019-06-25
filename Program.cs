de37bc174d55a937836b81bcd2c971841292f1cc
Shan <shanmitha555@gmail.com>
2019-06-25 15:31:57 +0530

    database first workflow

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntityFramework;
 
 namespace EntityFramework
 {
@@ -10,6 +11,23 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("starting the process");
+            using (var context = new TestDatabaseEntities() )
+            {
+                var blog = new Blog()
+                {
+                    PostId = 4,
+                    Title = "My 4th Post",
+                    Description = "My 4th Post",
+                    CreatedDate = DateTime.Today
+
+                };
+                context.Blogs.Add(blog);
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("Successfully added a post");
+            Console.ReadKey();
         }
     }
 }
